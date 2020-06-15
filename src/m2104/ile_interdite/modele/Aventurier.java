09d6ef544fa82a77500210c53f21ce7ce8316877
Vincenzo Carminati <vincenzo.carminati@etu.univ-grenoble-alpes.fr>
2020-06-15 10:57:48 +0200

    Creation des méthodes dans Aventurier

--- src/m2104/ile_interdite/modele/Aventurier.java
+++ src/m2104/ile_interdite/modele/Aventurier.java
@@ -19,7 +19,37 @@ public abstract class Aventurier {
     public void ajouterCarte(CarteTresor carte){
         main.add(carte);
     }
-    
+
+    public CarteTresor enleverCarte(int numCarte) {
+        return main.remove(numCarte);
+    }
+
+    public void changerPosition(int position) {
+        this.position = position;
+    }
+
+    public int[] deplacementPossible(Grille grille) {
+        ArrayList<Integer> deplacementPossible = new ArrayList<>();
+
+        if(position - 6 >= 0 && grille.getTuille(position-6).getEtat() != Etat.ABYSSE)
+            //si position - 6 (au dessus de l'aventurier) est dans la grille et que la tuile à cette position n'est pas dans l'abysse
+            deplacementPossible.add(position - 6);
+
+        if(position - 1 >= 0 && grille.getTuille(position - 1).getEtat() != Etat.ABYSSE)
+            //si la position - 1 (à gauche de l'aventurier) est dans la grille et que la tuile à cette position n'est pas dans l'abysse
+            deplacementPossible.add(position - 1);
+
+        if(position + 1 < 36 && grille.getTuille(position + 1).getEtat() != Etat.ABYSSE)
+            //si la position + 1 (à droite de l'aventurier est dans la grille et que la tuille à cette position n'est pas dans l'abysse
+            deplacementPossible.add(position + 1);
+
+        if(position + 6 < 36 && grille.getTuille(position + 6).getEtat() != Etat.ABYSSE)
+            //si la position + 6 (en dessous de l'aventurier) est dans la grille et que la tuille à cette position n'est pas dans l'abysse
+            deplacementPossible.add(position + 6);
+
+        return deplacementPossible.stream().mapToInt(i -> i).toArray(); //on converti l'ArrayList en tableau
+    }
+
     public int getPosition() {
         return position;
     }
